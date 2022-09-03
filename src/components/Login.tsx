c09439829605283a14398f036372ba27e18c1fca
Andrej Stojkovikj <andrejstojkovikj@yahoo.com>
2022-09-03 22:21:01 +0200

    Fixed a minor style error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -16,7 +16,7 @@ const Login = (props: any) => {
           <form action='#'>
             <div>
               <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900'>Your email</label>
-              <input type='email' name='email' id='email' className='bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2' placeholder='name@company.com' required />
+              <input type='email' name='email' id='email' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2' placeholder='name@company.com' required />
             </div>
             <div className='mt-2'>
               <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900'>Password</label>
